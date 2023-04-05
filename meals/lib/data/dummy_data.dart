365faf6f7bc07a18ca9044821ddf57e77c5a6fea
JhollRibeiro <jrsantos.joao@gmail.com>
2023-04-05 13:16:54 -0300

    Seção 7: Navegação & Múltiplas Telas - Flutter - Aula 180. Iniciando a APP

---
+++ meals/lib/data/dummy_data.dart
@@ -0,0 +1,57 @@
+
+import 'package:flutter/material.dart'; 
+import 'package:meals/models/category.dart';
+  
+// ignore: constant_identifier_names
+const DUMMY_CATEGORIES = [
+  Category(
+    id: 'c1',
+    title: 'Italiano',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Rápido & Fácil',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Alemã',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Leve & Saudável',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exótica',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Café da Manhã',
+    color: Colors.cyan,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asiática',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Francesa',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Verão',
+    color: Colors.teal,
+  ),
+];
